71e6287eb5599b7f2aa67c0c84fa7d7de1f69710
Jessica_Sugianto <jezicalove12@gmail.com>
2020-04-01 11:30:01 +0700

    mecoba menambahkan 3rd API

--- server/route/routeTodos.js
+++ server/route/routeTodos.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const { Router } = require('express')
 const router = Router()
 const Controller = require('../controllers/todo_controllers.js')
 const Authentication = require('../midlewares/authentication')
 const Authorization = require('../midlewares/authorization')
+const axios = require('axios')
 
 
 router.get('/', Authentication, Controller.findAll)
@@ -10,6 +12,18 @@ router.get('/', Authentication, Controller.findAll)
 router.get('/:id', Authentication, Authorization, Controller.getOne)
 
 router.post('/', Authentication, Controller.create)
+
+router.get('/whatsaap/:phone_number', (req, res) => {
+    const phone_number = req.params.phone_number
+    axios({
+        method: 'post',
+        url: 'https://eu11.chat-api.com/instance112338/',
+        data: {
+            "phone": phone_number,
+            "body": 'testing 3rd API'
+        }
+    })
+})
 
 router.put('/:id', Authentication, Authorization, Controller.update)
 
